f159e95e680fbe450e254acafb297e5812978fbb
Chase Burnham <cburnhamiv@gmail.com>
2018-09-28 12:17:29 -0400

    added fields to scanned license

---
+++ app/src/main/java/com/barscan/barscan/CameraCaptureActivity.java
@@ -0,0 +1,134 @@
+package com.barscan.barscan;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.wonderkiln.camerakit.CameraKitEventCallback;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+
+public class CameraCaptureActivity extends AppCompatActivity {
+
+    private static final String TAG = CameraCaptureActivity.class.getSimpleName();
+
+    public static final String LICENSE_PARAM = "licenseParam";
+
+    private CameraView cameraView;
+
+    private ScannedLicense mockLicense = new ScannedLicense("Carl", "Burnham", 23, "04/24/1995", "male", "", "11/20/2017 08:08:08");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_capture);
+
+        cameraView = findViewById(R.id.camera);
+
+        setupBarcodeScanner();
+    }
+
+
+    private void setupBarcodeScanner() {
+        FirebaseVisionBarcodeDetectorOptions options =
+                new FirebaseVisionBarcodeDetectorOptions.Builder()
+                        .setBarcodeFormats(
+                                FirebaseVisionBarcode.FORMAT_ALL_FORMATS)
+                        .build();
+    }
+
+    private void processImage(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector();
+
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                        parseImageResults(barcodes);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, e.getLocalizedMessage());
+                    }
+                });
+    }
+
+    private void parseImageResults(List<FirebaseVisionBarcode> barcodes) {
+        if(barcodes.isEmpty()) {
+            Toast.makeText(this, "No barcodes detected", Toast.LENGTH_SHORT);
+            returnDriverLicense(null);
+            return;
+        }
+        for (FirebaseVisionBarcode barcode: barcodes) {
+            Rect bounds = barcode.getBoundingBox();
+            Point[] corners = barcode.getCornerPoints();
+
+            String rawValue = barcode.getRawValue();
+
+            int valueType = barcode.getValueType();
+            // See API reference for complete list of supported types
+            switch (valueType) {
+                case FirebaseVisionBarcode.TYPE_DRIVER_LICENSE:
+                    FirebaseVisionBarcode.DriverLicense license = barcode.getDriverLicense();
+                    //returnDriverLicense(license);
+                    Log.e(TAG, license.getFirstName());
+                default:
+                    returnDriverLicense(null);
+
+            }
+        }
+    }
+
+
+    public void scanBarcodeClicked(View view) {
+        cameraView.captureImage(new CameraKitEventCallback<CameraKitImage>() {
+            @Override
+            public void callback(CameraKitImage cameraKitImage) {
+                processImage(cameraKitImage.getBitmap());
+            }
+        });
+    }
+
+    private void returnDriverLicense(FirebaseVisionBarcode.DriverLicense driverLicense) {
+        Intent data = new Intent();
+
+        data.putExtra(LICENSE_PARAM, mockLicense);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
+    }
+
+}
--- app/src/main/java/com/barscan/barscan/MainActivity.java
+++ app/src/main/java/com/barscan/barscan/MainActivity.java
@@ -1,44 +1,22 @@
 package com.barscan.barscan;
 
-import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.SparseIntArray;
-import android.view.Surface;
 import android.view.View;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
-import com.wonderkiln.camerakit.CameraKitEventCallback;
-import com.wonderkiln.camerakit.CameraKitImage;
-import com.wonderkiln.camerakit.CameraView;
 
-import java.util.List;
+import static com.barscan.barscan.CameraCaptureActivity.LICENSE_PARAM;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private CameraView cameraView;
+    private static final int TAKE_PHOTO_ACTION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,82 +24,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseApp.initializeApp(this);
-
-        cameraView = findViewById(R.id.camera);
-        setupBarcodeScanner();
     }
 
-    private void setupBarcodeScanner() {
-        FirebaseVisionBarcodeDetectorOptions options =
-                new FirebaseVisionBarcodeDetectorOptions.Builder()
-                        .setBarcodeFormats(
-                                FirebaseVisionBarcode.FORMAT_ALL_FORMATS)
-                        .build();
+
+    public void scanBarcodeClicked(View view) {
+        Intent intent = new Intent(this, CameraCaptureActivity.class);
+        startActivityForResult(intent, TAKE_PHOTO_ACTION);
     }
 
-    private void processImage(Bitmap bitmap) {
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
-        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
-                .getVisionBarcodeDetector();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
-                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
-                    @Override
-                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
-                        parseImageResults(barcodes);
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e(TAG, e.getLocalizedMessage());
-                    }
-                });
-    }
-
-    private void parseImageResults(List<FirebaseVisionBarcode> barcodes) {
-        for (FirebaseVisionBarcode barcode: barcodes) {
-            Rect bounds = barcode.getBoundingBox();
-            Point[] corners = barcode.getCornerPoints();
-
-            String rawValue = barcode.getRawValue();
-
-            int valueType = barcode.getValueType();
-            // See API reference for complete list of supported types
-            switch (valueType) {
-                case FirebaseVisionBarcode.TYPE_WIFI:
-                    String ssid = barcode.getWifi().getSsid();
-                    String password = barcode.getWifi().getPassword();
-                    int type = barcode.getWifi().getEncryptionType();
-                    break;
-                case FirebaseVisionBarcode.TYPE_URL:
-                    String title = barcode.getUrl().getTitle();
-                    String url = barcode.getUrl().getUrl();
-                case FirebaseVisionBarcode.TYPE_DRIVER_LICENSE:
-                    String name = barcode.getDriverLicense().getFirstName();
-                    Log.e(TAG, name);
-                    break;
+        if (requestCode == TAKE_PHOTO_ACTION) {
+            if (resultCode == RESULT_OK) {
+                ScannedLicense scannedLicense = data.getParcelableExtra(LICENSE_PARAM);
+                processBarcode(scannedLicense);
             }
         }
     }
-    public void scanBarcodeClicked(View view) {
-        cameraView.captureImage(new CameraKitEventCallback<CameraKitImage>() {
-            @Override
-            public void callback(CameraKitImage cameraKitImage) {
-                processImage(cameraKitImage.getBitmap());
-            }
-        });
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        cameraView.start();
-    }
+    private void processBarcode(ScannedLicense name) {
 
-    @Override
-    protected void onPause() {
-        cameraView.stop();
-        super.onPause();
     }
 }
