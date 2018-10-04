38590ff76aca83a88c795402bd75181249d01a27
Chase Burnham <cburnhamiv@gmail.com>
2018-10-03 23:58:19 -0400

    started login and removed apache library

--- app/src/main/java/com/barscan/barscan/CameraCaptureActivity.java
+++ app/src/main/java/com/barscan/barscan/CameraCaptureActivity.java
@@ -29,12 +29,7 @@ import com.wonderkiln.camerakit.CameraKitEventCallback;
 import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraView;
 
-import org.apache.commons.lang3.RandomStringUtils;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Random;
 
 public class CameraCaptureActivity extends AppCompatActivity {
 
@@ -160,13 +155,6 @@ public class CameraCaptureActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    private void pushMockData() {
-        for (int i = 0; i < 200; i++) {
-            ScannedLicense scannedId = getMockLicense();
-            pushData(scannedId);
-        }
-    }
-
     private void pushData(ScannedLicense scannedId) {
         String uid = mDatabase.child("customers").push().getKey();
         scannedId.setUuid(uid);
@@ -189,13 +177,6 @@ public class CameraCaptureActivity extends AppCompatActivity {
 
         progress_spinner.setVisibility(View.GONE);
         showDialog(scannedLicense);
-    }
-
-    private ScannedLicense getMockLicense() {
-        Random random = new Random();
-        String gender = random.nextBoolean() ? "male" : "female";
-        return new ScannedLicense(RandomStringUtils.randomAlphanumeric(5), RandomStringUtils.randomAlphanumeric(8), random.nextInt(40) + 18, "04/24/1995",
-                gender, "", DateHelper.getRandomTime());
     }
 
 
